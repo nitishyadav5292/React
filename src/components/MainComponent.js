0fef8bb73274bc0d20525e6f1305c9be6216ee45
Nitish Yadav <nitishyadav5292@gmail.com>
2021-12-31 16:54:31 +0530

    SPA Part-2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -22,7 +22,15 @@ class Main extends Component{
         promotions: PROMOTIONS
         }
     }
+
   render(){
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+  
     const HomePage = () => {
       return(
         <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
@@ -36,6 +44,7 @@ class Main extends Component{
         <Switch>
           <Route path="/home" component={HomePage}></Route>
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}></Route>
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route path="/contactus" component={Contact} />
           <Redirect to="/home"></Redirect>
         </Switch>
