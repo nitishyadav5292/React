98e894e9e2049420ac903666a5666b7b8759e949
Nitish Yadav <nitishyadav5292@gmail.com>
2022-01-03 15:36:31 +0530

    intro to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,6 +12,8 @@ import {COMMENTS} from '../shared/comments';
 import {PROMOTIONS} from '../shared/promotions';
 import {LEADERS} from '../shared/leaders';
 import {Route, Switch, Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
 
 class Main extends Component{
     constructor(props){
@@ -25,6 +27,15 @@ class Main extends Component{
     }
 
   render(){
+    const mapStateToProps = state => {
+      return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+      }
+    }
+
     const DishWithId = ({match}) => {
       return(
           <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
@@ -34,11 +45,21 @@ class Main extends Component{
   
     const HomePage = () => {
       return(
-        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-        leader={this.state.leaders.filter((leader)=> leader.featured)[0]}
-        promotion={this.state.promotions.filter((promo)=> promo.featured)[0]} />
-      )
+          <Home 
+              dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+    
     return (
       <div className="App">
         <Header />
@@ -56,4 +77,4 @@ class Main extends Component{
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
